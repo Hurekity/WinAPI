113cd23e416d9883ba47862f6bb73972084ba10e
Garawanta Hurekity <Astronomyc9@gmail.com>
2023-12-12 17:55:49 +0900

    uncompleted

---
+++ Concatenator/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_DIALOG_MAIN                 101
+#define IDI_ICON1                       103
+#define IDI_ICON_NOTEBOOK               103
+#define IDC_STATIC_FULLNAME             1001
+#define IDC_EDIT_FULLNAME               1002
+#define IDC_EDIT_CHAR                   1003
+#define IDC_STATIC_CHAR                 1004
+#define IDC_CHECK_ATTENTION             1006
+#define IDC_CHECK_GOOD_UNDERSTANDING    1008
+#define IDC_CHECK_GOOD_CODING           1009
+#define IDC_CHECK_NO_HOMEWORKS          1010
+#define IDC_CHECK_BAD_ATTENDANCE        1011
+#define IDC_STATIC_ADVANTAGES           1014
+#define IDC_STATIC_DISADVANTAGES        1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
